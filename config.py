1c0727fdb2924a9e877ef2e5c4b70cfb5ace4c5d
Vitaly Karpenko <vitkarpenko@gmail.com>
2016-05-04 14:48:48 +0300

    dir && ind nodes

--- config.py
+++ config.py
@@ -2,11 +2,16 @@ import numpy as np
 import scipy.spatial
 import matplotlib.pyplot as plt
 
+# координаты углов пластинки
 x_min = 0
 x_max = 1
 y_min = 0
 y_max = 1
 
+# на сколько частей делится каждая сторона пластинки при построении сетки
 grid_density = 5
+
+# общее количество узлов
+nNod = grid_density ** 2
 
 timesteps = 10
--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from config import *
 
 
-def triangulate_rectangle(x_min, x_max, y_min, y_max, grid_density):
-    points = np.dstack((np.linspace(x_min, x_max, grid_density), np.full(grid_density, y_min, dtype='float')))[0]
+def generate_nodes(x_min, x_max, y_min, y_max, grid_density):
+    '''Разделяет пластинку на равномерную прямоугольную сетку.
+    grid_density шагов на каждой стороне.'''
+    nodes = np.dstack((np.linspace(x_min, x_max, grid_density),
+                        np.full(grid_density, y_min, dtype='float')))[0]
     for i in range(1, grid_density):
         c = i / (grid_density - 1)
-        points = np.concatenate((points, np.dstack((np.linspace(x_min, x_max, grid_density), np.full(grid_density, y_min + (y_max - y_min) * c, dtype='float')))[0]), axis=0)
-    tri = scipy.spatial.Delaunay(points)
-    plt.triplot(points[:,0], points[:,1], tri.simplices.copy())
-    plt.plot(points[:,0], points[:,1], 'o')
-    plt.show()
+        nodes = np.concatenate((nodes, np.dstack((np.linspace(x_min, x_max, grid_density),
+                                                    np.full(grid_density, y_min + (y_max - y_min) * c,
+                                                                dtype='float')))[0]), axis=0)
+    return nodes
 
 
-triangulate_rectangle(x_min, x_max, y_min, y_max, grid_density)
+def triangulate_rectangle(nodes):
+    '''Триангулирует пластинку по заданным узлам методом Делоне.'''
+    tri = scipy.spatial.Delaunay(nodes)
+    return tri.simplices.copy()
+
+
+# dir_nodes - номера узлов с условием Дирихле, в данном случае внешние
+# ind_nodes - номера свободных, т.е. в данном случае внутренних узлов
+nodes_coords = generate_nodes(x_min, x_max, y_min, y_max, grid_density)
+dir_nodes = np.array([i + grid_density * j for j in range(grid_density) for i in range(grid_density)
+                                                                if i == 0 or i == grid_density - 1
+                                                                or j == 0 or j == grid_density - 1])
+ind_nodes = np.array([x for x in range(grid_density ** 2) if x not in dir_nodes])
+
+triangles = triangulate_rectangle(nodes_coords)
